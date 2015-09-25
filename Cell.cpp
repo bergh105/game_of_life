e290b8a13212c4f84cbaf826539bedd5ab9eb22b
titte101 <titte101@mail.chapman.edu>
2015-09-25 15:56:34 -0700

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -13,22 +13,24 @@ Cell::~Cell()
 
 }
 
-void Cell::numNeighbors(int **arr, int **arrB, size_t w, size_t h)
+int Cell::numNeighborsClassic(int **arr, int **arrB, size_t w, size_t h)
 {
 	int neighborCount = 0;
+	int stableCount = 0;
 	cout<< 1;
 	for(size_t i=1; i<(w-1); ++i) {
 		cout << 2;
 		for(size_t j=1; j<(h-1); ++j) {
 			cout << 3;
-			
+			//this line causes segmentation fault (11) **probably too much memory being used
 			neighborCount = arr[i-1][j] + arr[i-1][j-1] + arr[i-1][j+1] + arr[i][j+1] + arr[i][j-1] + arr[i+1][j+1] + arr[i+1][j] + arr[i+1][j-1];
 			
-			
-					
-			
+			//even just this causes segmentation fault: 11
+			cout << "hey";			
+			//if(arr[i][j] == 1)
+			//	++neighborCount;
 
-			//Cell Death
+			
 			if(neighborCount <2 || neighborCount >= 4) {
 				arrB[i][j] = 0;
 			}
@@ -39,14 +41,16 @@ void Cell::numNeighbors(int **arr, int **arrB, size_t w, size_t h)
 				else if (arr[i][j] == 1)
 					arrB[i][j] = 1;
 			}
-			//the beauty of life
 			else if(neighborCount == 3) {
 				arrB[i][j] = 1;
 			} 
+			if (arr[i][j] != arrB[i][j])
+				stableCount++;
 			neighborCount = 0;
 			
 		}
 	}
-	
+	  
+	return stableCount;
 }
 
