3595a21b7c9265382a260ce389d48dc265c1de89
bergh105 <bergh105@mail.chapman.edu>
2015-09-27 16:41:07 -0700

    Update main.cpp w changes regarding file input

--- main.cpp
+++ main.cpp
@@ -1,100 +1,164 @@
 #include <iostream>
-//#include "cell.h"
+#include <string>
+#include <math.h>
+#include <fstream>
+//#include <array>
+#include "Cell.h"
+
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
-	int rows, columns;
-	cout << "How many rows? " << endl;
-	cin >> rows;
-	cout << "How many columns? " << endl;
-	cin >> columns;
 
-	int width, height;
-	//set width and height of board to account for border cells
-	if(columns > rows) {
-		width = columns+2;
-		height = rows+2;
-		cout << "Width: " << width << " Height: " << height << endl;
-	}
-	else if (rows > columns) {
-		width = rows+2;
-		height = columns+2;
-		cout << "Width: " << width << " Height: " << height << endl;
-	}
-	//create board
-	int** board = new int*[width];
-	for(int i=0; i<width; ++i) {
-		board[i] = new int[height];
-	}
-	
-	//initialize all cells to empty
-	for (int i=0; i<height; ++i) {
-		for (int j=0; j<width; ++j) {
-			board[i][j] = 0;
+	Cell c;
+
+	cout << "Would you like to enter a map file, or be provided a random assignment? (map/random)" << endl;
+
+	string response;
+	getline(cin, response);
+
+	if(response == "random") {
+
+		int rows, columns;
+		cout << "How many rows? " << endl;
+		cin >> rows;
+		cout << "How many columns? " << endl;
+		cin >> columns;
+		
+		int width, height;
+		//set width and height of board to account for border cells
+		if(columns >= rows) {
+			width = columns+2;
+			height = rows+2;
+			cout << "Width: " << width << " Height: " << height << endl;
 		}
-	}
-
-	//populate some cells (indices chosen randomly)
-	//in order for these indices to work, input 4 rows, 5 columns
-	board[1][2] = 1;
-	board[1][4] = 1;
-	board[2][2] = 1;
-	board[2][4] = 1;
-	board[3][1] = 1;
-	board[3][2] = 1; 
-
-	//create copy board to populate next gen
-	int** boardB = new int*[width];
-	for(int i=0; i<width; ++i) {
-		boardB[i] = new int[height];
-	}
-	for (int i=0; i<height; ++i) {
-		for (int j=0; j<width; ++j) {
-			boardB[i][j] = 0;
+		else if (rows > columns) {
+			width = rows+2;
+			height = columns+2;
+			cout << "Width: " << width << " Height: " << height << endl;
 		}
-	}
 
-	//check neighbors, alter boardB accordingly
-	int neighborCount = 0;
+		//create board
+		int** board = new int*[width];
+		for(int i=0; i < width; ++i) {
+			board[i] = new int[height];
+		}
 
-	//1 so that first border column and row are skipped. width-1 so that last column skipped, height-1 so that last row skipped
-	for(int i=1; i<height-1; ++i) {
-		for(int j=1; j<width-1; ++j) {
-			
-			neighborCount = board[i-1][j] + board[i-1][j-1] + board[i-1][j+1] + board[i][j+1] + board[i][j-1] + board[i+1][j+1] + board[i+1][j] + board[i+1][j-1];
-			cout << "neighbor count: " << neighborCount << endl;
-			if(neighborCount <2 || neighborCount >= 4) {
-				boardB[i][j] = 0;
+		//initialize all cells to empty
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				board[i][j] = 0;
 			}
-			else if(neighborCount == 2) {
-				//cell state must remain the same. check what current state is, retain that state
-				if (board[i][j] == 0)
-					boardB[i][j] = 0;
-				else if (board[i][j] == 1)
-					boardB[i][j] = 1;
-			}
-			else if(neighborCount == 3) {
-				boardB[i][j] = 1;
-			} 
-			
 		}
-	}
+
+		//prompt population density
+		cout << "Enter desired population density (number between 0 and 1): " << endl;
+		double popD;
+		//set pop density
+		cin >> popD;
+
+		//error checking
+		while(!(popD>=0 || popD<1)) {
+			cout << "Invalid. Re-enter: " << flush;
+			cin >> popD;
+		}
 	
-	//print values of all indexes of boardB to check
-	for (int i=0; i<width; ++i) {
-		for (int j=0; j<height; ++j) {
-			cout << boardB[i][j] << endl;
+		//populate cells
+		c.initialPopClassic(board, width, height, popD);
+
+	
+		cout << "No segmentation error yet!" << endl;
+		
+		//create copy board
+		int** boardB = new int*[width];
+		for(int i=0; i<width; ++i) {
+			boardB[i] = new int[height];
+		} 
+		
+		//check neighbors, alter boardB accordingly
+		
+		c.numNeighborsClassic(board, boardB, width, height);
+		
+		//print values of all indexes of boardB to check
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				cout << boardB[i][j] << endl;
+			}
 		}
+
+		//Delete arrays to prevent Segmentation Fault	
+		for (int i = 0; i<height; ++i)
+		{		
+			delete board[i];
+			delete boardB[i];	
+		}
+		delete board;
+		delete boardB;
+
 	}
 
-	for (int i =0; i<height; ++i) {
-		delete [] board[i];
-		delete [] boardB[i];
+	else if(response == "map") {
+
+		//prompt user for file path to create grid
+		cout << "Type desired file name: " << endl;
+
+		string fileName;
+		getline(cin, fileName);
+
+		//test to make sure file has opened. If not, re-enter file name.
+		ifstream file;
+		file.open(fileName);
+		if (!file.is_open()) {
+			cout << "Error opening file." << endl;
+			cout << "Re-type file name: " << endl;
+			getline(cin, fileName);
+		}
+		else {
+			file.close();
+		}
+
+		
+		file.open(fileName);
+
+		//set height and width (1st and second lines of file, respectively)
+		int rows, columns;
+		getline(file, columns);
+		getline(file, rows);
+
+		int width, height;
+		//set width and height of board to account for border cells
+		if(columns >= rows) {
+			width = columns+2;
+			height = rows+2;
+			cout << "Width: " << width << " Height: " << height << endl;
+		}
+		else if (rows > columns) {
+			width = rows+2;
+			height = columns+2;
+			cout << "Width: " << width << " Height: " << height << endl;
+		}
+
+		//create board
+		int** board = new int*[width];
+		for(int i=0; i < width; ++i) {
+			board[i] = new int[height];
+		}
+
+		//initialize all cells to empty
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				board[i][j] = 0;
+			}
+		}
+
+		c.initialPopClassic_file(board, fileName);
 	}
-	delete [] board;
-	delete [] boardB;
-	
+	else {
+		cout << "Please type either 'random' or 'map': " << endl;
+		getline(cin, response);
+	}
+
 	return 0;
 }
+
