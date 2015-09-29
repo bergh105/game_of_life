5d71d73688fa7d1c408e82634f6d65543702e538
titte101 <titte101@mail.chapman.edu>
2015-09-29 01:10:06 -0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <fstream>
+
+
 
 using namespace std;
 
@@ -8,17 +9,25 @@ class Cell
 	public:
 		Cell();
 		virtual ~Cell();
-		//bool flip = false;
+		
 		double popDensity;
 		int numCells;
-		void numNeighborsClassic(int **arr, int **arrB, int w, int h);
-		void numNeighborsDonut(int **arr, int **arrB, int w, int h);
-		void numNeighborsMirror(int **arr, int **arrB, int w, int h);
-		void initialPop(int **arr, int w, int h, double d);
-		void initialPopFile(int **arr, string fileName);		
-		void printConsole(int **arrB, int w, int h);
-		void printFlipConsole(int **arrB, int w, int h);
-		void printFile(int **arrB, int w, int h, string fileName);
+		
+		
+		
+
 		void Quit();
+		void printFlipConsole(int **arrB, int w, int h);
+		void printConsole(int **arrB, int w, int h);
+
+		void printFlipFile(int **arrB, int w, int h);
+		void printFile(int **arrB, int w, int h);
+
+		void numNeighborsClassic(int **arr, int **arrB, int w, int h, bool fType, bool out);
+		void numNeighborsMirror(int **arr, int **arrB, int w, int h, bool fType, bool out);
+		void numNeighborsDonut(int **arr, int **arrB, int w, int h, bool fType, bool out);
+		
+		void initialPop(int **arr, int w, int h, double d);
+		void initialPop_file(int **arr, const char* fileName);
 
 };
