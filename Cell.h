79faf1a24fc0f0dbe1a80d3a1d13a70c04107c0e
titte101 <titte101@mail.chapman.edu>
2015-09-28 17:02:15 -0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 
 using namespace std;
 
@@ -8,19 +8,17 @@ class Cell
 	public:
 		Cell();
 		virtual ~Cell();
-		
+		//bool flip = false;
 		double popDensity;
 		int numCells;
-		int numNeighborsClassic(int **arr, int **arrB, size_t w, size_t h);
-		int numNeighborDonut(int **arr, int **arrB, size_t w, size_t h);
-		int numNeighborMirror(int **arr, int **arrB, size_t w, size_t h);  
-		
-		
-		
-		
-		
-		
-		
-		
+		void numNeighborsClassic(int **arr, int **arrB, int w, int h);
+		void numNeighborsDonut(int **arr, int **arrB, int w, int h);
+		void numNeighborsMirror(int **arr, int **arrB, int w, int h);
+		void initialPop(int **arr, int w, int h, double d);
+		void initialPopFile(int **arr, string fileName);		
+		void printConsole(int **arrB, int w, int h);
+		void printFlipConsole(int **arrB, int w, int h);
+		void printFile(int **arrB, int w, int h, string fileName);
+		void Quit();
 
 };
