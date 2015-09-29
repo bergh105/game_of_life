7d9728663904bb1ebcdbd5b82bd47f12adda48e3
bergh105 <bergh105@mail.chapman.edu>
2015-09-28 19:22:51 -0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 
 using namespace std;
@@ -11,8 +12,19 @@ class Cell
 		
 		double popDensity;
 		int numCells;
-		void numNeighbors(int **arr, int **arrB, size_t w, size_t h);
+		int numNeighborsClassic(int **arr, int **arrB, size_t w, size_t h);
 		void ReadIn();
 		void turn(char **bA, char **bB);
-		int PlayGame();
+
+		void Quit();
+		void printFlipConsole(int **arrB, int w, int h);
+		void printConsole(int **arrB, int w, int h);
+
+		void numNeighborsClassic(int **arr, int **arrB, int w, int h);
+		void numNeighborsMirror(int **arr, int **arrB, int w, int h);
+		void numNeighborsDonut(int **arr, int **arrB, int w, int h);
+		
+		void initialPop(int **arr, int w, int h, double d);
+		void initialPop_file(int **arr, string fileName);
+
 };
