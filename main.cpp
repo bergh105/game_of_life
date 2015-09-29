dde9834a01d6861e00f0db44996926f6201fcbc7
titte101 <titte101@mail.chapman.edu>
2015-09-28 17:01:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-//#include <array>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <fstream>
 #include "Cell.h"
 
 
@@ -9,80 +12,91 @@ int main(int argc, char **argv) {
 
 
 	Cell c;
+	cout << "Would you like to enter a map file, or be provided a random assignment? (map/random)" << endl;
 
-	int rows, columns;
-	bool flip = false;
-	cout << "How many rows? " << endl;
-	cin >> rows;
-	cout << "How many columns? " << endl;
-	cin >> columns;
+	string response;
+	getline(cin, response);
 
-	//set width and height of board to account for border cells
-	
-	int width, height;
-//set width and height of board to account for border cells
-	if(columns > rows) {
-		width = columns+2;
-		height = rows+2;
-		cout << "Width: " << width << " Height: " << height << endl;
-		flip = true;
-	}
-	else if (rows > columns) {
-		width = rows+2;
-		height = columns+2;
-		cout << "Width: " << width << " Height: " << height << endl;
-		flip = false;
-	}
-	cout << "Width: " << width << " Height: " << height << endl;
+	if(response == "random") {
 
-	//create board
-	int** board = new int*[width];
-	for(int i=0; i < width; ++i) {
-		board[i] = new int[height];
-	}
-
-	//initialize all cells to empty
-	for (int i=0; i<width; ++i) {
-		for (int j=0; j<height; ++j) {
-			board[i][j] = 0;
+		int rows, columns;
+		cout << "How many rows? " << endl;
+		cin >> rows;
+		cout << "How many columns? " << endl;
+		cin >> columns;
+		
+		int width, height;
+		//set width and height of board to account for border cells
+		if(columns >= rows) {
+			width = columns+2;
+			height = rows+2;
+			cout << "Width: " << width << " Height: " << height << endl;
+			//flip = false;
 		}
-	}
-	
-	cout << "No segmentation error yet!" << endl;
-
-	//populate some cells (indices chosen randomly)
-	//in order for these indices to work, input 4 rows, 5 columns
-	
-	//create copy board
-	int** boardB = new int*[width];
-	for(int i=0; i<width; ++i) {
-		boardB[i] = new int[height];
-	}
-	
-	for (int i =0; i<width; ++i)
-	{
-	
-	}
-	cout << "No segmentation error yet!" << endl;
-	//check neighbors, alter boardB accordingly
-	
-	c.numNeighborsClassic(board, boardB, width, height);
-	
-	//print values of all indexes of boardB to check
-	for (int i=0; i<width; ++i) {
-		for (int j=0; j<height; ++j) {
-			cout << boardB[i][j] << endl;
+		else if (rows > columns) {
+			width = rows+2;
+			height = columns+2;
+			cout << "Width: " << width << " Height: " << height << endl;
+			//flip = true;
 		}
-	}
-	for (int i = 0; i<height; ++i)
-	{		
-		delete board[i];
-		delete boardB[i];	
-	}
-	delete board;
-	delete boardB;
+
+		//create board
+		int** board = new int*[width];
+		for(int i=0; i < width; ++i) {
+			board[i] = new int[height];
+		}
+
+		//initialize all cells to empty
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				board[i][j] = 0;
+			}
+		}
+
+		//prompt population density
+		cout << "Enter desired population density (number between 0 and 1): " << endl;
+		double popD;
+		//set pop density
+		cin >> popD;
+
+		//error checking
+		while(!(popD>=0 || popD<1)) {
+			cout << "Invalid. Re-enter: " << flush;
+			cin >> popD;
+		}
+	
+		//populate cells
+		c.initialPop(board, width, height, popD);
 
 	
+		cout << "No segmentation error yet!" << endl;
+		
+		//create copy board
+		int** boardB = new int*[width];
+		for(int i=0; i<width; ++i) {
+			boardB[i] = new int[height];
+		}
+		
+		//check neighbors, alter boardB accordingly
+		//c.numNeighborsClassic(board, boardB, width, height);
+		//c.numNeighborsDonut(board, boardB, width, height);
+		c.numNeighborsMirror(board, boardB, width, height);
 
+		//Delete arrays to prevent Segmentation Fault	
+		for (int i = 0; i<height; ++i)
+		{		
+			delete board[i];
+			delete boardB[i];	
+		}
+		delete board;
+		delete boardB;
+		c.Quit();
+	}
+	
+	
+	
+	
+
+	
 	return 0;
 }
