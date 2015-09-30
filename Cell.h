0f3b9bcd6d6503c37170a83506818e93a0d958ac
titte101 <titte101@mail.chapman.edu>
2015-09-29 18:10:04 -0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -20,12 +20,12 @@ class Cell
 		void printFlipConsole(int **arrB, int w, int h);
 		void printConsole(int **arrB, int w, int h);
 
-		void printFlipFile(int **arrB, int w, int h);
-		void printFile(int **arrB, int w, int h);
+		void printFlipFile(int **arrB, int w, int h, bool looping);
+		void printFile(int **arrB, int w, int h, bool looping);
 
-		void numNeighborsClassic(int **arr, int **arrB, int w, int h, bool fType, bool out);
-		void numNeighborsMirror(int **arr, int **arrB, int w, int h, bool fType, bool out);
-		void numNeighborsDonut(int **arr, int **arrB, int w, int h, bool fType, bool out);
+		void numNeighborsClassic(int **arr, int **arrB, int w, int h, bool fType, bool out, bool Loop);
+		void numNeighborsMirror(int **arr, int **arrB, int w, int h, bool fType, bool out, bool Loop);
+		void numNeighborsDonut(int **arr, int **arrB, int w, int h, bool fType, bool out, bool Loop);
 		
 		void initialPop(int **arr, int w, int h, double d);
 		void initialPop_file(int **arr, const char* fileName);
